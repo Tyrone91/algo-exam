ee369cca0f890776e24bd9f808ed0ac8172c949e
Tyrone <tweetyandsilwester@hotmail.de>
2019-01-07 21:23:39 +0100

    fixed udraw connection added ui

--- src/main/java/assignment3/Main.java
+++ src/main/java/assignment3/Main.java
@@ -1,26 +1,134 @@
 package assignment3;
 
-import assignment3.RoBDD.RoBDDTest;
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.util.Arrays;
+import java.util.List;
+
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class Main {
 
     public static final String MY_LINUX_PATH = "/home/tyrone/dev/hs/algo-exam/uDrawGraph-3.1/bin/";
     public static final String MY_WINDOWS_PATH = "c:/dev/tools/uDraw(Graph)/bin/";
+    
+    static interface GraphSupplier {
+        
+        public String getGraphAsString(UDrawConnector connector);
+        public void onActivation(JPanel container);
+        
+    }
+    
+    static class ControlWindow extends JFrame {
+        
+        private UDrawConnector m_Connector;
+        private GraphSupplier m_CurrentSupplier;
+        private List<GraphSupplier> m_GraphSuppliers;
+        
+        public ControlWindow(UDrawConnector connector, GraphSupplier...graphSuppliers ) {
+            m_GraphSuppliers = Arrays.asList(graphSuppliers);
+            m_Connector = connector;
+            setTitle("UDraw Control");
+            setLayout(new BorderLayout());
+            setSize(200, 400);
+            final JPanel content = new JPanel(new GridLayout(0, 1));
+            
+            JButton connectBttn = new JButton("Connect");
+            connectBttn.addActionListener( e -> {
+                connector.connect();
+                connectBttn.setEnabled(false);
+            });
+            
+            m_Connector.onDisconnect( () -> {
+                connector.end();
+                connectBttn.setEnabled(true);
+            });
+            
+            JButton sendGraphBttn = new JButton("Send Graph");
+            sendGraphBttn.addActionListener( e -> {
+                connector.send(m_CurrentSupplier.getGraphAsString(connector));
+            });
+            
+            final JPanel graphContainer = new JPanel();
+            
+            JComboBox<GraphSupplier> graphSelection = new JComboBox<>(graphSuppliers);
+            m_CurrentSupplier = graphSuppliers[0];
+            graphSelection.addActionListener( e -> {
+                m_CurrentSupplier = (GraphSupplier)graphSelection.getSelectedItem();
+                graphContainer.removeAll();
+                m_CurrentSupplier.onActivation(graphContainer);
+            });
+            
+            content.add(connectBttn);
+            content.add(graphSelection);
+            content.add(graphContainer);
+            content.add(sendGraphBttn);
+            
+            add(content, BorderLayout.CENTER);
+            
+            init();
+            setVisible(true);
+        }
+        
+        private void init() {
+            addWindowListener( new WindowAdapter() {
+                @Override
+                public void windowClosing(WindowEvent e) {
+                    m_Connector.end();
+                    dispose();
+                }
+            });
+            
+            
+        }
+        
+    }
 
     public static void main(String[] args) {
         //PatriciaTree.test();
         //RedBlackTree.test();
-        RoBDD.test();
-        /*
-        UDrawConnector u = new UDrawConnector(MY_LINUX_PATH + "uDrawGraph");
+        //RoBDD.test();
+        
+        UDrawConnector u = new UDrawConnector(MY_WINDOWS_PATH + "uDrawGraph");
         final String graph = u.newGraph(
                 u.newNode("1"),
                 u.newNode("2"),
                 u.newNode("3")
                 );
+        
+        u.send(graph);
+        ControlWindow w  = new ControlWindow(u, new DebugGraph1());
+        
+        
+        System.out.println("program over");
+    }
+    
+    static class DebugGraph1 implements GraphSupplier {
 
-        u.send(graph); 
-       */
+        @Override
+        public String getGraphAsString(UDrawConnector u) {
+            return u.newGraph(
+                    u.newNode("1"),
+                    u.newNode("2"),
+                    u.newNode("3")
+                    );
+        }
+
+        @Override
+        public void onActivation(JPanel container) {
+            
+        }
+        
+        @Override
+        public String toString() {
+            return this.getClass().getSimpleName();
+        }
+        
     }
 }
 
--- src/main/java/assignment3/UDrawConnector.java
+++ src/main/java/assignment3/UDrawConnector.java
@@ -1,10 +1,9 @@
 package assignment3;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.Arrays;
 import java.util.LinkedList;
@@ -17,29 +16,39 @@ public class UDrawConnector implements Runnable {
     private static final int UDRAW_PORT = 2542;
     
     private Socket m_Socket;
-    private BufferedWriter m_Output;
+    private PrintWriter m_Writer;
     private BufferedReader m_Input;
     private Process m_Process;
     private Queue<String> m_Messages = new LinkedList<>();
+    private String m_UdrawPath;
+    private Runnable m_OnDisconnect = () -> {};
     
     public UDrawConnector(final String appPath) {
+        m_UdrawPath = appPath;
+    }
+    
+    public void connect() {
         try{
-            System.out.format("using: '%s'\n", appPath);
-            ProcessBuilder p = new ProcessBuilder(appPath, "-server");
+            System.out.println("udraw path:_" + m_UdrawPath);
+            ProcessBuilder p = new ProcessBuilder(m_UdrawPath, "-server");
             m_Process = p.start();
             m_Socket = new Socket("127.0.0.1", UDRAW_PORT);
-            m_Output = new BufferedWriter( new OutputStreamWriter(m_Socket.getOutputStream()));
+            m_Writer = new PrintWriter(m_Socket.getOutputStream(), true);
+            //m_Output = new BufferedWriter( new OutputStreamWriter(m_Socket.getOutputStream() ));
             m_Input = new BufferedReader(new InputStreamReader(m_Socket.getInputStream()));
+            run();
         } catch(Exception e) {
             e.printStackTrace(System.err);
         }
     }
     
     public void end() {
+        System.out.println("ending");
         try {
-            m_Output.close();
-            m_Socket.close();
-            m_Process.destroy();
+            send("menu(file(close))");
+            if(m_Writer != null) m_Writer.close();
+            if(m_Socket != null) m_Socket.close();
+            if(m_Process != null) m_Process.destroy();
         } catch (IOException e) {
             e.printStackTrace(System.err);
         }
@@ -56,9 +65,16 @@ public class UDrawConnector implements Runnable {
                 
     }
     
+    public void setUDrawPath(String path) {
+        m_UdrawPath = path;
+    }
+    
     private void send2(String msg) {
         try {
-            m_Output.write(msg);
+            System.out.println("sending: " + msg);
+            //m_Output.write(msg);
+            //m_Output.flush();
+            m_Writer.println(msg);
         } catch (Exception e) {
             e.printStackTrace(System.err);
         }
@@ -67,12 +83,20 @@ public class UDrawConnector implements Runnable {
     public synchronized void send(String msg) {
         //m_Messages.add(msg);
         if(ready) {
-            System.out.println("sending: " + msg);
             send2(msg);
+            ready = false;
         } else {
-            System.out.println("waiting");
+            System.out.println("waiting: " + msg);
             m_Messages.add(msg);
         }
+    }
+    
+    public void forceSend(String msg) {
+        send2(msg);
+    }
+    
+    public void onDisconnect(Runnable run) {
+        m_OnDisconnect = run;
     }
         
     
@@ -85,18 +109,24 @@ public class UDrawConnector implements Runnable {
                 try {
                     String line = null;
                     while( (line = m_Input.readLine()) != null ) {
-                        System.out.println("reading");
-                        System.out.println(line);
+                        System.out.println("reading: " + line);
                         if(!ready && "ok".equals(line)) {
                             ready = true;
-                            m_Messages.forEach(this::send);
+                            if(!m_Messages.isEmpty() ) {                                
+                                send(m_Messages.poll());
+                            }
+                        }
+                        
+                        if("disconnect".equals(line)) {
+                            m_OnDisconnect.run();
                         }
                     }
                     System.out.println("ending reading");
                 } catch(Exception e) {
                     System.out.println(e);
+                    m_OnDisconnect.run();
                 }
-                
+                System.out.println("thread over");
             }).start();
             
         } catch(Exception e) {
